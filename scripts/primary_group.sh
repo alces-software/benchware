e6b6ffd636ef01f1fe2707b454adca2f2a68565e
stuts <stu.j.franks@gmail.com>
2018-09-27 11:23:48 +0100

    Split meta nodeattr to scripts

---
+++ scripts/primary_group.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Vars
+node="$1"
+
+# Check for nodeattr
+command -v nodeattr > /dev/null
+if [ $? == 1 ] ; then
+  module load services/pdsh
+  if [ $? == 1 ] ; then
+    echo "nodeattr command missing"
+  fi
+fi
+
+nodeattr -l $node |paste -s -d ',' |cut -d',' -f1
+
